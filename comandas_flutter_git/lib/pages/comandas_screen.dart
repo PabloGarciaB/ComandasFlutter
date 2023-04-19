92acb949c15c26cec348eb5d83c3658e0506e05c
PabloGarciaB <53867090+PabloGarciaB@users.noreply.github.com>
2023-04-19 01:12:24 -0600

    TODO FUNCIONAL YAAAAAAAAAS
    letsgeit 1:12am, 19 de abril

---
+++ comandas_flutter_git/lib/pages/comandas_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Comandas extends StatefulWidget {
+  const Comandas({super.key});
+
+  @override
+  State<Comandas> createState() => _ComandasState();
+}
+
+class _ComandasState extends State<Comandas> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
